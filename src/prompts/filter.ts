046fb7a688714d2797389752bea031f3827ae8c5
Steve Korshakov <steve@korshakov.com>
2023-11-24 16:44:01 -0800

    fix: return empty line completition, avoid completition when system autocomplete shown

--- src/prompts/filter.ts
+++ src/prompts/filter.ts
@@ -4,11 +4,16 @@ export function isSupported(doc: vscode.TextDocument) {
     return doc.uri.scheme === 'file';
 }
 
-export function isNotNeeded(doc: vscode.TextDocument, position: vscode.Position): boolean {
+export function isNotNeeded(doc: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext): boolean {
 
     // Avoid autocomplete on empty lines
-    const line = doc.lineAt(position.line).text.trim();
-    if (line.trim() === '') {
+    // const line = doc.lineAt(position.line).text.trim();
+    // if (line.trim() === '') {
+    //     return true;
+    // }
+
+    // Avoid autocomplete when system menu is shown (ghost text is hidden anyway)
+    if (context.selectedCompletionInfo) {
         return true;
     }
 
